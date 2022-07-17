5344b957ca3fd08da47f9e6907e1fff275e6a781
TomatoFT <thomas.lezard.2002@gmail,com>
2022-07-17 15:04:25 +0700

    New Update

---
+++ lalala.r
@@ -0,0 +1,105 @@
+rm(list = ls())
+
+# readable in HTML format
+library(xml2)
+library(httr)
+library(rvest)
+library(stringr)
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(plyr)
+
+#===============================================================================
+# base
+
+
+data <- GET("https://www.metasrc.com/stats")
+
+content <- content(data, as = "text")
+html_data <- read_xml(content, as_html = TRUE)
+
+#==============================================================================
+# get all of values of filter
+attr_value_mode <- html_data %>% html_nodes("#mode option") %>% html_attr("value")
+mode <- html_data %>% html_nodes("#mode option") %>% html_text()
+
+attr_value_patch <- html_data %>% html_nodes("#patch option") %>% html_attr("value")
+patch <- html_data %>% html_nodes("#patch option") %>% html_text()
+
+attr_value_region <- html_data %>% html_nodes("#region option") %>% html_attr("value")
+region <- html_data %>% html_nodes("#region option") %>% html_text()
+
+df_mode = cbind(attr_value_patch, patch)
+df_mode = data.frame(df_mode)
+patch <- substr(dataset$Name[index],6,nchar(dataset$Name[index]))
+
+setwd("D:/DS103-Project/dataset")
+
+for (i in 1:length(df_mode$attr_value_patch)){
+    print(df_mode$attr_value_patch[i])
+    url <- sprintf("https://www.metasrc.com/5v5/%s/stats",df_mode$attr_value_patch[i])
+    print(url)
+    data <- GET(url)
+    
+    # df_mode$patch[i]=substr(df_mode$patch[i],6,nchar(dataset$Name[index]))
+    
+    print(df_mode$patch[i])
+    patch_column = df_mode$attr_value_patch[i]
+    name_csv = df_mode$attr_value_patch[i]
+    
+    content <- content(data, as = "text")
+    html_data <- read_xml(content, as_html = TRUE)
+    
+    table_head <- xml_text(xml_find_all(html_data, "//*[@id='table-scroll']/table/thead/tr/th"))
+    
+    table_data <- xml_text(xml_find_all(html_data,"//*[@id='table-scroll']/table/tbody/tr/td"))
+    # So cot o day tuong ung so luong table_head
+    # So dong o day tuong ung so luong table_data
+    
+    dataset <- matrix(ncol=length(table_head), nrow = 
+                        (length(table_data)/ length(table_head) ))
+    data_col = length(table_head)
+    # Lan luot lay tung dong trong table_data bo vao dataset
+    
+    count = 1
+    i = 1
+    while(i<length(table_data)-data_col){
+      dataset[count,] <- c(table_data[i: (i-1+data_col)])
+      i = i+ data_col
+      count = count +1
+    }
+    
+    # Chuyen ma tran dataset thanh dang dataframe
+    
+    dataset <- as.data.frame(dataset)
+    dataset <- na.omit(dataset)
+    
+    # gan tieu de cho dataframe
+    
+    names(dataset) <- c(table_head)
+    
+
+    
+    name = sprintf("data_LoL_%s_stats.csv",name_csv)
+    
+    write.csv(dataset, name, row.names = FALSE) }
+
+print("-------------------Done with full data-------------------")
+
+#Delete the file to update
+
+if (file.exists("Data_LoL_Full_patches.csv")) {
+  file.remove("Data_LoL_Full_patches.csv")
+  print("The file is deleted")
+}
+
+#import and merge all three CSV files into one data frame
+
+df <- list.files(path='D:/DS103-Project/dataset') %>% 
+  lapply(read_csv, show_col_types = FALSE) %>% 
+  bind_rows 
+
+
+write.csv(df,file = "Data_LoL_Full_patches.csv")
+
